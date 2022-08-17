bacf36e45f58ff61e4e269a1cc758e8a2908c46a
Alex <ajm.dosreis.daponte@gmail.com>
2022-08-17 17:20:16 +0200

    Added animation to Main component

--- apps/admin/src/components/layout/Main.tsx
+++ apps/admin/src/components/layout/Main.tsx
@@ -10,27 +10,36 @@ import {
   CategoryCreate,
 } from '../../pages';
 import { routes } from '../../utils/routes';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 
-export const Main = () => (
-  <GridItem
-    area={'main'}
-    overflowY={'scroll'}
-    overflowX={'hidden'}
-    bg={useColorModeValue('gray.100', 'gray.900')}
-    paddingBottom="20"
-  >
-    <Routes>
-      <Route path={routes['home'].url} element={<Home />} />
-      <Route path={routes['artworks'].url} element={<Outlet />}>
-        <Route index element={<ArtworksList />} />
-        <Route path={routes['artworks'].children?.['id'].url} element={<ArtworkEdit />} />
-        <Route path={routes['artworks'].children?.['create'].url} element={<ArtworkCreate />} />
-      </Route>
-      <Route path={routes['categories'].url} element={<Outlet />}>
-        <Route index element={<CategoriesList />} />
-        <Route path={routes['categories'].children?.['id'].url} element={<CategoryEdit />} />
-        <Route path={routes['categories'].children?.['create'].url} element={<CategoryCreate />} />
-      </Route>
-    </Routes>
-  </GridItem>
-);
+export const Main = () => {
+  const [animationParent] = useAutoAnimate<HTMLDivElement>();
+
+  return (
+    <GridItem
+      area={'main'}
+      overflowY={'scroll'}
+      overflowX={'hidden'}
+      bg={useColorModeValue('gray.100', 'gray.900')}
+      paddingBottom="20"
+      ref={animationParent}
+    >
+      <Routes>
+        <Route path={routes['home'].url} element={<Home />} />
+        <Route path={routes['artworks'].url} element={<Outlet />}>
+          <Route index element={<ArtworksList />} />
+          <Route path={routes['artworks'].children?.['id'].url} element={<ArtworkEdit />} />
+          <Route path={routes['artworks'].children?.['create'].url} element={<ArtworkCreate />} />
+        </Route>
+        <Route path={routes['categories'].url} element={<Outlet />}>
+          <Route index element={<CategoriesList />} />
+          <Route path={routes['categories'].children?.['id'].url} element={<CategoryEdit />} />
+          <Route
+            path={routes['categories'].children?.['create'].url}
+            element={<CategoryCreate />}
+          />
+        </Route>
+      </Routes>
+    </GridItem>
+  );
+};
